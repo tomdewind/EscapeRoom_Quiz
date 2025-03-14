3efd33cf44662870baf2bcb43b09b57d0f2acc04
Tom <tom@dewind.nl>
2025-03-14 10:25:06 +0100

    Media Player is kinda working!

--- site.js
+++ site.js
@@ -6,8 +6,12 @@ const MediaPlayer = document.getElementById("MediaPlayer");
 const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-const pepernoot = document.getElementsByClassName("close")[0];
-const span = document.getElementsByClassName("ok-btn")[0];
+const modal1close = document.getElementsByClassName("close")[0];
+const modal1ok = document.getElementsByClassName("ok-btn")[0];
+
+// Get the video and the play button
+const play = document.getElementsByClassName("play")[0];
+const video = document.getElementsByClassName("video")[0];
 
 
 
@@ -17,14 +21,19 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
+// When the user clicks on the play button, start the video
+play.onclick = function() {
+  video.play();
+}
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+modal1ok.onclick = function() {
   modal.style.display = "none";
   MediaPlayer.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-pepernoot.onclick = function() {
+modal1close.onclick = function() {
     modal.style.display = "none";
 }
 
