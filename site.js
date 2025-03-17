3a57bf71889251be44d2ef5a502bbfe3a6cb0e24
Tom <tom@dewind.nl>
2025-03-17 12:23:05 +0100

    Savestate

--- site.js
+++ site.js
@@ -1,24 +1,33 @@
 // Get the modal
-const modal = document.getElementById("myModal");
+const StartDialog = document.getElementById("StartDialog");
 const MediaPlayer = document.getElementById("MediaPlayer");
 
 // Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+const Start = document.getElementById("Start");
+const Launch = document.getElementById("LaunchMediaPlayer");
 
 // Get the <span> element that closes the modal
-const modal1close = document.getElementsByClassName("close")[0];
-const modal1ok = document.getElementsByClassName("ok-btn")[0];
+const StartModalClose = document.getElementsByClassName("close")[0];
+const StartModalOK = document.getElementsByClassName("start-modal-proceed")[0];
 
 // Get the video and the play button
 const play = document.getElementsByClassName("play")[0];
 const video = document.getElementsByClassName("video")[0];
 
 
-
+// Hide elements on start
+function onLoadFunctions() {
+}
+window.onload = onLoadFunctions;
+  StartDialog.style.display = "none";
+  MediaPlayer.style.display = "none";
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+Start.onclick = function() {
+  StartDialog.style.display = "block";
+}
+Launch.onclick = function() {
+  MediaPlayer.style.display = "block";
 }
 
 // When the user clicks on the play button, start the video
@@ -26,15 +35,14 @@ play.onclick = function() {
   video.play();
 }
 
-// When the user clicks on <span> (x), close the modal
-modal1ok.onclick = function() {
-  modal.style.display = "none";
-  MediaPlayer.style.display = "block";
+// When the user clicks on OK, close the modal
+StartModalOK.onclick = function() {
+  StartDialog.style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
-modal1close.onclick = function() {
-    modal.style.display = "none";
+// When the user clicks on (x), close the modal
+StartModalClose.onclick = function() {
+  StartDialog.style.display = "none";
 }
 
 // let af en toe veranderen
